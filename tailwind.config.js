ce9c47e36d1494150bdc67bc555849427f89ba0a
Renan-Lira <renan.almeida@lojaspintos.com.br>
2023-04-11 10:07:23 -0300

    tailwindcss colors add

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,27 @@ module.exports = {
       },
     },
 
+    colors: {
+      'transparent': 'transparent',
+      'white': 'white',
+      'gray': {
+        100: '#F5F5F5',
+        300: '#E1E1E1',
+        400: '#E8E8E8',
+        450: '#A2A2A2',
+        500: '#D9D9D9',
+        600: '#4D4D4D',
+        800: '#8A8A8A',
+      },
+      'gray-transparent': '#00000054',
+      'blue': {
+        100: '#E5F5FF',
+        200: '#0099FA',
+        800: '#00447D'
+      },
+
+    },
+
     fontFamily: {
       'sans': ['Montserrat', 'sans-serif'],
     },

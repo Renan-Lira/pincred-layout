87d64f1b5a182917c59e7bf0f8fbfe163ce1ddaa
Renan-Lira <renan.almeida@lojaspintos.com.br>
2023-04-14 12:44:52 -0300

    readme

--- readme.md
+++ readme.md
@@ -1,7 +1,26 @@
 # Layout Pincred
 
+
 ## compilar projeto
 >``` bash
 > npm install
 > npm run compile 
 >```
+
+## Documentação
+
+### Modals
+> #### São gerados a partir de um script `./src/js/modalEnable.js`
+>
+> Funcionam a partir de dataset no html
+>
+> > `data-modal-target` = id do modal a ser exibido
+> >
+> > `data-modal-close` = id do modal a ser fechado
+>
+> Esses `dataset` devem estar nos botões que irão ser utilizados
+>
+> **todo modal deve começar com `width = 0`** 
+>
+> dessa forma a lógica é implementada para que tudo funcione normalmente
+
--- src/js/filesManager.js
+++ src/js/filesManager.js
@@ -16,3 +16,27 @@ const camOpen = (idInput) => {
 
     input.click()
 }
+
+const inputcontroller = (ev) => {
+    let buttonRemove = ev.parentElement.children[1]
+
+    if (ev.value) {
+        buttonRemove.style.display = 'flex'
+        buttonsRemoveFile(ev)
+    } else {
+        buttonRemove.style.display = 'none'
+    }
+
+
+}
+
+const buttonsRemoveFile = (input) => {
+    let buttons = document.querySelectorAll('.remove-file')
+    
+    buttons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            input.value = ''
+            button.style.display = 'none'
+        })
+    })
+}

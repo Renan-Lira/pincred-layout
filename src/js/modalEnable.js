7757afb8ba093fd5736399e7c7270999cce8f81a
Renan-Lira <renan.almeida@lojaspintos.com.br>
2023-04-22 10:24:09 -0300

    modal melhorias

--- src/js/modalEnable.js
+++ src/js/modalEnable.js
@@ -2,11 +2,12 @@
 const main = (params) => {
     // função dinamica para qualquer tipo de modal existente no projeto
     // botões e modais serão identificado pelo dataset "modal-..."
-    const buttonsModal = document.querySelectorAll('button[data-modal-target]')
+    const buttonsModal = document.querySelectorAll('[data-modal-target]')
 
     buttonsModal.forEach((button, k) => {
 
         // adicionando funcionamento para cada botão
+        button.style.cursor = 'pointer'
         button.addEventListener('click', () => {
             let modal = document.querySelector(`#${button.dataset.modalTarget}`)
             let scrollTarget = modal.children.item(0)
@@ -17,6 +18,7 @@ const main = (params) => {
             // ativação dos botões para fechar o modal
             let buttonsClose = scrollTarget.contentDocument.body.querySelectorAll('[data-modal-close]')
             buttonsClose.forEach((close, index) => {
+                close.style.cursor = 'pointer'
                 close.addEventListener('click', () => modal.style.width = "0")
             })
         },{passive: true})

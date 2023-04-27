435bb0ca889360c097c0b1961d7a7077a0e0b52d
Renan-Lira <renan.almeida@lojaspintos.com.br>
2023-04-27 11:49:27 -0300

    produtos page create

--- src/js/menu-dropdown.js
+++ src/js/menu-dropdown.js
@@ -1,8 +1,19 @@
-const menulist = document.getElementById('menu-list')
 
-function clickButtonMenu() {
-    
+
+
+document.querySelector('#button-menu').addEventListener('click', () => {
+    const menulist = document.getElementById('menu-list')
+
     if (menulist.classList.contains('menu-visible')) return menulist.classList.remove(['menu-visible'])
     
     return menulist.classList.add(['menu-visible'])
-}
+})
+
+
+document.querySelector('#button-sub-menu-produtos').addEventListener('click', () => {
+    const menulist = document.getElementById('menu-list-produtos')
+
+    if (menulist.classList.contains('menu-visible')) return menulist.classList.remove(['menu-visible'])
+    
+    return menulist.classList.add(['menu-visible'])
+})

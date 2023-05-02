4ef111f8e40a21ab6f63ffa758c33950a047e8c8
Renan-Lira <renan.almeida@lojaspintos.com.br>
2023-05-02 08:37:44 -0300

    add hrefs

--- src/js/menu-dropdown.js
+++ src/js/menu-dropdown.js
@@ -10,15 +10,6 @@ document.querySelector('#button-menu').addEventListener('click', () => {
 })
 
 
-
-let typeClickSubMenu = ''
-
-if ('ontouchstart' in window) {
-    typeClickSubMenu = 'click'    
-} else {
-    typeClickSubMenu = 'mouseover'
-}
-
 document.querySelector('#button-sub-menu-produtos').addEventListener('click', () => {
 
     const menulist = document.getElementById('menu-list-produtos')
